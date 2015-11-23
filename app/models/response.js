2cd8f1b6da6856e18bc3802ab4170a8c6dd8b8c8
Paul Wilson <paul@vermonster.com>
2015-11-23 18:05:15 -0500

    Simplify Responses Model and remove boilerplate files

--- app/models/response.js
+++ app/models/response.js
@@ -1,6 +1,5 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  time: DS.attr('date'),
   answer: DS.attr('value')
 });
--- app/router.js
+++ app/router.js
@@ -6,7 +6,6 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('responses');
   this.route('questions');
 });
 
